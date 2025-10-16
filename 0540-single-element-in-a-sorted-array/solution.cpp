afb34569ff371f5aa92e13f048d66c5ac7505e41
Manjunath <manjumanjuyadav33@gmail.com>
2025-10-16 14:20:31 +0000

    [LeetCode Sync] Runtime - 15 ms (3.10%), Memory - 31.3 MB (6.47%)

---
+++ 0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++)
+        {
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return 0;
+        
+    }
+};
