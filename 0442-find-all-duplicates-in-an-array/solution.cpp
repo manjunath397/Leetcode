3304ea4671091817768f35299c4d17e156066ea5
Manjunath <manjumanjuyadav33@gmail.com>
2025-10-18 18:02:40 +0000

    [LeetCode Sync] Runtime - 65 ms (8.49%), Memory - 67.2 MB (7.38%)

---
+++ 0442-find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++)
+        {
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second>=2)
+            {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+        
+    }
+};
