1f8c8eb1272e8694d1610e0ff605745d4ef0a86c
Manjunath <manjumanjuyadav33@gmail.com>
2026-01-18 14:39:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 117.9 MB (93.76%)

---
+++ 0234-palindrome-linked-list/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+        ListNode* fast=head;
+        ListNode*slow=head;
+        while(fast && fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode*prev =nullptr;
+        ListNode* curr=slow;
+        while(curr)
+        {
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        ListNode* first=head;
+        ListNode* second=prev;
+        while(second)
+        {
+            if(first->val != second->val) return false;
+            second=second->next;
+            first=first->next;
+        }
+        return true;
+
+        
+    }
+};
