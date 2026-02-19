dcfcfaab6252954491bdeffc33d38ddb6a6b0b07
Manjunath <manjumanjuyadav33@gmail.com>
2026-02-19 01:58:34 +0000

    [LeetCode Sync] Runtime - 4 ms (20.68%), Memory - 20.5 MB (98.88%)

---
+++ 0136-single-number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(auto c:nums)
+        {
+            ans^=c;
+
+        }
+        return ans;
+        
+    }
+};
